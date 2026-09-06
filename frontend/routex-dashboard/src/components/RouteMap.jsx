960c22845baccb56a64410191a3234e4ed67e781
parag-sanghavi <paragnsanghavi@gmail.com>
2026-09-06 14:47:39 +0530

    Revert "Adjust route map zoom"
    This reverts commit e1d9c94b517d15b623a1ebd56d77d2556022a56d.

--- frontend/routex-dashboard/src/components/RouteMap.jsx
+++ frontend/routex-dashboard/src/components/RouteMap.jsx
@@ -21,7 +21,7 @@ function RouteMap({ geometry }) {
     <div className="route-map-container">
       <MapContainer
         center={KOTHRUD_CENTER}
-        zoom={17}
+        zoom={15}
         scrollWheelZoom={true}
         className="route-map"
       >
